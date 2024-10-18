74852e4c97e890a0962fbacea8deeeebcb8dbd0d
Awino <estherojul53@gmail.com>
2024-10-18 15:04:33 +0300

    upd-3

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/", (req,res)=>{
     res.render("index.ejs")
 })
 
+app.get("/signin", (req,res)=>{
+    res.render("signin.ejs")
+})
+
+app.post("/register", (req,res)=>{
+    res.render("register.ejs")
+})
+
 
 
 //page not found
